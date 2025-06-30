32553f7f80a52b8769425c7d153ed92db773f7a7
lijiahao <lijiahao@cmschina.com.cn>
2025-06-30 10:49:47 +0800

    update console icon

--- src/components/ConsoleItem.tsx
+++ src/components/ConsoleItem.tsx
@@ -4,12 +4,45 @@ import {Card, Text, Button} from 'react-native-paper';
 import {useTranslation} from 'react-i18next';
 import {SvgXml} from 'react-native-svg';
 import dayjs from 'dayjs';
+import {getSettings} from '../store/settingStore';
 import icons from '../common/svg';
 
 const ConsoleItem = (props: any) => {
   const {t} = useTranslation();
+  const settings = getSettings();
+  const theme = settings.theme ?? 'dark';
 
   const consoleItem = props.consoleItem;
+
+  let apName = consoleItem.apName as string;
+  let isPS5 = false;
+  let isPS5Pro = false;
+
+  if (apName) {
+    apName = apName.toLocaleUpperCase();
+    if (apName.indexOf('PS5') > -1) {
+      isPS5 = true;
+
+      if (apName.indexOf('PRO') > -1) {
+        isPS5Pro = true;
+      }
+    }
+  }
+
+  let icon = isPS5 ? (isPS5Pro ? icons.PS5Pro : icons.PS5) : icons.ConsoleIcon;
+  let imageStyle = isPS5 ? styles.image : {};
+  let iconHeight = 80;
+  if (isPS5) {
+    iconHeight = 100;
+  }
+
+  if (theme !== 'dark') {
+    icon = isPS5
+      ? isPS5Pro
+        ? icons.PS5ProLight
+        : icons.PS5Light
+      : icons.ConsoleIcon;
+  }
 
   return (
     <Card style={styles.wrap}>
@@ -18,13 +51,8 @@ const ConsoleItem = (props: any) => {
           <Text variant="titleLarge" style={styles.textCenter}>
             {consoleItem.serverNickname}
           </Text>
-          <View style={styles.image}>
-            <SvgXml
-              // xml={theme === 'dark' ? icons.ConsoleDark : icons.ConsoleLight}
-              xml={icons.ConsoleIcon}
-              width={'100%'}
-              height={80}
-            />
+          <View style={imageStyle}>
+            <SvgXml xml={icon} width={'100%'} height={iconHeight} />
           </View>
           {consoleItem.consoleId && (
             <Text variant="labelSmall" style={styles.textCenter}>
@@ -88,7 +116,10 @@ const ConsoleItem = (props: any) => {
 const styles = StyleSheet.create({
   wrap: {},
   consoleInfos: {},
-  image: {},
+  image: {
+    transform: [{rotate: '180deg'}],
+    marginLeft: -20,
+  },
   textCenter: {
     textAlign: 'center',
   },
